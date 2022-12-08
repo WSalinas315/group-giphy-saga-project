62006b2f1f2c0914561e20271749aa631590c00c
Billy Salinas <wsalinas315@gmail.com>
2022-12-08 13:48:41 -0600

    moved API GET to its own router file

---
+++ server/routes/search.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { default: axios } = require('axios');
+const router = express.Router();
+require('dotenv').config();
+
+// Route to GET gifs of a specified theme from Giphy API
+app.use('/:query', (req, res) => {
+  // extract search term from url
+  const querySubject = req.params.query;
+
+  // make axios get with giphy API
+  axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${querySubject}&limit=8`)
+  .then((response) => {
+    console.log('Success with Giphy API GET!');
+    res.send(response.data);
+  })
+  .catch((error) =>{
+    console.log('Error with Giphy API GET:', error);
+    res.sendStatus(500);
+  });
+})
+
+module.exports = router;
