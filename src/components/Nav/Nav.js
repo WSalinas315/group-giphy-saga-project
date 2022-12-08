3b23f87e5ca2336576d24601d78286eb27f28c35
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-12-08 14:55:13 -0600

    finished header and nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,22 @@
+import { Link } from 'react-router-dom';
 export default function Nav() {
-    return(
-         <nav className="nav">
-        <a href="/" className="Favorite Images">G
-)
-}
+    return (
+        
+      <nav className="nav">
+         <ul>
+            <li>
+        <Link to="/" >
+          Search
+        </Link>
+        </li>
+
+        
+        <li>
+          <Link to="/favorites" >
+          Favorites
+        </Link>
+        </li>
+        </ul>
+      </nav>
+    )
+  }
