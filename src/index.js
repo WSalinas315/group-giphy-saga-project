809ecc62835e8ee5aa2236a3c193607be021f968
August McAllister <august.chm@gmail.com>
2022-12-08 10:57:28 -0600

    Sagas, first reducer added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,25 @@ import { Provider } from 'react';
 import logger from 'redux-logger';
 import { takeEvery, put } from 'redux-saga/effects';
 
+function* rootSaga() {
 
+}
+
+const searchResults = (state = [], action) => {
+  switch(action.type) {
+    case 'SET_SEARCH_RESULTS': return action.payload;
+    case 'CLEAR_SEARCH_RESULTS': return [];
+    default: return state;
+  }
+}
+
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+const reduxStore = createStore({}, applyMiddleware(logger, sagaMiddleware))
+
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
 <Provider store={store}>
