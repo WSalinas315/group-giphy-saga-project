a5b9fdf900343fcb6f63181c332c26de0b5ec0d2
August McAllister <august.chm@gmail.com>
2022-12-08 14:02:55 -0600

    added search bar

---
+++ src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import SearchBar from '../SearchBar/Searchbar'
+import SearchResultsList from '../SearchResultsList/SearchResultsList'
+
+
+export default function Search() {
+
+
+  return (
+  <div>
+    <h3>Search for GIFs:</h3>
+    <SearchBar />
+    <SearchResultsList />
+  </div>
+  )
+}
