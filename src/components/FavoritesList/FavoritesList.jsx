a03d7b4753331a2bfb469013bebf1bd41274c91c
August McAllister <august.chm@gmail.com>
2022-12-08 14:11:35 -0600

    Implemented favorites list architecture & placeholders

--- src/components/FavoritesList/FavoritesList.jsx
+++ src/components/FavoritesList/FavoritesList.jsx
@@ -4,14 +4,20 @@ import FavoriteListItem from "../FavoriteListItem/FavoriteListItem";
 
 export default function FavoritesList() {
 
+  const favoriteGifs = useSelector(store => store.favoriteGifs)
 
-
-
+  const favoritesList = favoriteGifs.map(item => {
+    return (
+      <FavoriteListItem item={item} />
+    )
+  })
 
   return (
     <div>
-      <p>List of favorites here</p>
-
+      <p>List of favorites:</p>
+      <div className="favorites-display">
+      {favoritesList}
+      </div>
     </div>
   )
 }
