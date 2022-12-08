585594b90ad81dc2cb285dbba31b0db02f77fb9f
August McAllister <august.chm@gmail.com>
2022-12-08 15:33:20 -0600

    added dispatch action to result item

--- src/components/SearchResultsItem/SearchResultsItem.jsx
+++ src/components/SearchResultsItem/SearchResultsItem.jsx
@@ -4,20 +4,34 @@ export default function SearchResultsItem({ item }) {
 
   const dispatch = useDispatch();
 
-  return (
-    <div className="search-result-card">
-      
-      <img
-        className="search-img"
-        src={item.images.downsized.url} 
-        alt={item.title} />
-      
-      <button className="material-symbols-outlined fav-btn">
-        favorite
-      </button>
-      
-      <div className="search-card-info">
+  const handleClickFav = () => {
+    console.log('fav button clicked');
 
+    dispatch({
+      type: 'ADD_TO_FAVORITES',
+      payload: item.images.downsized.url
+    })
+  }
+
+  return (
+    <div className="search-container">
+
+      <div className="search-result-card">
+        
+        <img
+          className="search-img"
+          src={item.images.downsized.url} 
+          alt={item.title} />
+        
+        <button 
+          className="material-symbols-outlined fav-btn"
+          onClick={handleClickFav}>
+          favorite
+        </button>
+        
+        <div className="search-card-info">
+
+        </div>
       </div>
     </div>
   )
