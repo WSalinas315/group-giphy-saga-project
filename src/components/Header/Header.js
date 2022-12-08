2c5cd39a4fcca6f02b7471adbcd289e5d3cb604d
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-12-08 13:54:55 -0600

    working on the nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Giphy Saga Project</h1>
+        </header>
+    )
+    
+}
+export default Header;
