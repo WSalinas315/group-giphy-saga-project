9c2f23ded8b8d3bd18f85607943e89b735e15e09
Billy Salinas <wsalinas315@gmail.com>
2022-12-09 11:59:32 -0600

    category drop downs added, not entirely functional

--- src/components/FavoriteListItem/FavoriteListItem.jsx
+++ src/components/FavoriteListItem/FavoriteListItem.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 export default function FavoriteListItem({ item }) {
+
+  const dispatch = useDispatch();
+
+   const categories = useSelector(store => store.categories);
+
+  const categoriesList = categories.data.map(category => {
+    return (
+      <option value={category.name} key={category.id}>{category.name}</option>
+    )
+  })
+
+  const updateCategory = (event) => {
+    dispatch({type: 'SET_CATEGORY', payload: {id: item.id, category_id: event}});
+  }
 
   return (
     <div className="favorites-container">
@@ -9,6 +25,15 @@ export default function FavoriteListItem({ item }) {
       <div className="favorites-card">
         <img className="favorites-img" src={item.url} alt={item.title} />
         <p>{item.title}</p>
+        {console.log('Categories: ',categories)}
+
+        <form>
+          <label>Set Category</label>
+          <select id="category" name="category" onChange={(event) => updateCategory(event.target.key)}>
+            {categoriesList}
+          </select> 
+        </form>
+
       </div>
     </div>
   )
