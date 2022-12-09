1d643465cc49549976a7dbb8b8e6e92f377bfc35
Billy Salinas <wsalinas315@gmail.com>
2022-12-09 09:48:11 -0600

    dispatch and return written

--- src/components/FavoriteListItem/FavoriteListItem.jsx
+++ src/components/FavoriteListItem/FavoriteListItem.jsx
@@ -1,13 +1,25 @@
 import { useDispatch } from "react-redux";
-
+import React, {useEffect} from "react";
 
 export default function FavoriteListItem({ item }) {
 
+  // dispatch
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  const getFavorites = () =>{
+    dispatch({type: 'FETCH_FAVORITES'});
+  }
+
   return (
-    <div>
-      <p>Favorite list item placeholder. Gif URL: {item.url}</p>
+    <div className="favorites-container">
+      <div className="favorites-card">
+        <img className="favorites-img" src={item.url} alt={item.title} />
+        <p>{item.title}</p>
+      </div>
     </div>
   )
 }
